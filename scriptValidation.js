1e7f121b8da7ae4c18bbafe2aa512c1971842728
BaharehTorki <bahareh.torki@yh.nackademin.se>
2023-03-27 16:24:48 +0200

    Adding scriptValidation.js Adding checkoutForm.html Adding login.html Adding signUp.html

---
+++ scriptValidation.js
@@ -0,0 +1,50 @@
+
+const form = document.querySelector('#form');
+const firstName = document.querySelector('#firstName');
+const lastName = document.querySelector('#lastName');
+const email = document.querySelector('#email');
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    checkInputs();
+    console.log("Success")
+})
+
+const checkInputs = () => {
+
+    const fistNameValue = firstName.value.trim()
+    console.log(fistNameValue)
+    if (fistNameValue.length >= 2 && firstNameValue.length <= 50) {
+
+        setSuccess(firstName)
+    } else {
+        console.log(firstName)
+        setError(firstName, 'Your name should be min 2 letter and max 50 letter')
+    }
+
+
+    const emailValue = email.value.trim()
+    console.log(emailValue)
+
+    if (emailValue.includes('@')){
+        setSuccess(email)
+    }else {
+        console.log(email)
+        setError(email, 'Your email should include @ and be max 50 letter')
+    }
+
+}
+
+const setError = (input, message) => {
+    const formControl = input.parentElement
+    console.log(formControl)
+    const small = formControl.querySelector('small')
+
+    formControl.className = 'form-control error'
+    small.innerText = message
+}
+const setSuccess = (input) => {
+    const formControl = input.parentElement
+    formControl.className = 'form-control success'
+}
